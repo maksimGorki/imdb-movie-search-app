2e221c7941a268f13daf0e0138f9d3d12e1c7a46
gorkem <grkemarpaci@gmail.com>
2021-04-25 00:30:35 +0300

    switching from http to https request

--- src/components/SearchMovies.jsx
+++ src/components/SearchMovies.jsx
@@ -6,19 +6,19 @@ import LoadingPage from "./LoadingPage";
 import useData from "./useData";
 
 const getApiEndpoint = (query, page) => {
-  const url = `http://www.omdbapi.com/?&apikey=9e1134ca&s=${query}&page=${page}`;
+  const url = `https://www.omdbapi.com/?&apikey=9e1134ca&s=${query}&page=${page}`;
   return url;
 };
 
 const SearchMovies = () => {
   const [query, setQuery] = useState("");
-  const { searching, data: movies } = useData(query, getApiEndpoint);
+  const { isLoading, data: movies } = useData(query, getApiEndpoint);
 
   return (
     <>
       <SearchBox query={query} setQuery={setQuery} />
       <div className="horizontalRule"></div>
-      {searching ? <LoadingPage /> : <MovieDeck movies={movies} />}
+      {isLoading ? <LoadingPage /> : <MovieDeck movies={movies} />}
     </>
   );
 };
